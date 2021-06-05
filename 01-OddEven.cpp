c03e2d73627638eba97def04382d3070a63c38cb
SUBASH N <84781041+SubashN02@users.noreply.github.com>
2021-06-05 15:11:38 +0530

    Add files via upload
    These are c++ programs performed on 29-05-2021 as Exercise 1

---
+++ 01-OddEven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter a number"<<"\n";
+    cin>>number;
+    if(number%2==0)
+    {
+
+        cout<<"The entered number is even";
+    }
+    else
+    {
+        cout<<"The entered number is odd";
+    }
+    return 0;
+}
